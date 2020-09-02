c37ca7e4d154afa1b921aa881fb0f99f8d5fc58d
Audrey Thomasson <audrey@macbook-pro.lan>
2020-09-02 15:56:55 -0500

    blue boxes appear

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,7 @@ console.log("you got this -main.js linked")
 // Import and invoke the ticket booth component function
 
 import {TicketBooth} from "./TicketBooth.js";
+import { RideTicketHolders } from "./rides/RideTicketHolders.js";
 
 TicketBooth();
+RideTicketHolders();
